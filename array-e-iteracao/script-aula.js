d2cce206c8c2b2adb246047ac5f2da9f1c782042
GabrielH-NH <gabrielhenriquenh@gmail.com>
2023-11-17 21:19:53 -0300

    array e iteração

--- array-e-iteracao/script-aula.js
+++ array-e-iteracao/script-aula.js
@@ -199,3 +199,95 @@ const maiorValor = numeros.reduce((anterior, atual) => {
 });
 
 maiorValor; // 60
+
+
+
+
+// PASSO A PASSO REDUCE
+// Passo a passo do método reduce criando um Objeto.
+
+// 1
+//aulas.reduce(({}, {nome: 'HTML 1', min: 15}, 0) => {
+//  {}[0] = 'HTML 1';
+//  return {0: 'HTML 1'};
+//}, {})
+
+// 2
+//aulas.reduce(({0: 'HTML 1'}, {nome: 'HTML 2', min: 10}, 1) => {
+//  {0: 'HTML 1'}[1] = 'HTML 2';
+//  return {0: 'HTML 1', 1: 'HTML 2'};
+//}, {})
+
+// 3
+//aulas.reduce(({0: 'HTML 1', 1: 'HTML 2'}, {nome: 'CSS 1', min: 20}, 2) => {
+//  {0: 'HTML 1', 1: 'HTML 2'}[2] = 'CSS 1';
+//  return {0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1'};
+//}, {})
+
+// 4
+//aulas.reduce(({0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1'}, {nome: 'JS 1', min: 25}, 3) => {
+//  {0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1'}[3] = 'JS 1';
+//  return {0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1', 3: 'JS 1'};
+//}, {})
+
+
+
+
+// [].REDUCERIGHT()
+// Existe também o método [].reduceRight(), a diferença é que este começa a iterar da direita para a esquerda, enquanto o reduce itera da esquerda para a direita.
+
+const frutas = ['Banana', 'Pêra', 'Uva'];
+
+const frutasRight = frutas.reduceRight((acc, fruta) => acc + ' ' + fruta);
+const frutasLeft = frutas.reduce((acc, fruta) => acc + ' ' + fruta);
+
+frutasRight; // Uva Pêra Banana
+frutasLeft; // Banana Pêra Uva
+
+
+
+
+// [].SOME()
+// [].some(), se pelo menos um return da iteração for truthy, ele retorna true.
+
+const frutas2 = ['Banana', 'Pêra', 'Uva'];
+const temUva = frutas2.some((fruta) => {
+  return fruta === 'Uva';
+}); // true
+
+function maiorQue100(numero) {
+  return numero > 100;
+}
+const numeros3 = [0, 43, 22, 88, 101, 2];
+const temMaior = numeros3.some(maiorQue100); // true
+
+
+
+
+// [].EVERY()
+// [].every(), se todos os returns das iterações forem truthy, o método irá retornar true. Se pelo menos um for falsy, ele irá retornar false.
+
+const frutas3 = ['Banana', 'Pêra', 'Uva', ''];
+// False pois pelo menos uma fruta
+// está vazia '', o que é um valor falsy
+const arraysCheias = frutas3.every((fruta) => {
+  return fruta; // false
+});
+
+const numeros4 = [6, 43, 22, 88, 101, 29];
+const maiorQue3 = numeros4.every(x => x > 3); // true
+
+
+
+
+
+// [].FIND() E [].FINDINDEX()
+// [].find(), retorna o valor atual da primeira iteração que retornar um valor truthy. Já o [].findIndex(), ao invés de retornar o valor, retorna o index deste valor na array.
+
+const frutas4 = ['Banana', 'Pêra', 'Uva', 'Maçã'];
+const buscaUva = frutas4.findIndex((fruta) => {
+  return fruta === 'Uva'; 
+}); // 2
+
+const numeros5 = [6, 43, 22, 88, 101, 29];
+const buscaMaior45 = numeros5.find(x => x > 45); // 88
