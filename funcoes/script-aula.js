7a1d2a80f2f3224637582d394c3cf2cf520e88bb
GabrielH-NH <gabrielhenriquenh@gmail.com>
2024-01-03 20:50:55 -0300

    fazendo alguns exercicios

--- funcoes/script-aula.js
+++ funcoes/script-aula.js
@@ -141,3 +141,86 @@ Math.max.call(null, 3, 4, 5, 6, 7, 20);
 
 
 
+//APPLY VS CALL
+// A única diferença é a array como segundo argumento.
+
+const li4 = document.querySelectorAll('li');
+
+function itemPossuiAtivo(item) {
+  return item.classList.contains('ativo');
+}
+
+const filtro1 = Array.prototype.filter.call(li4, itemPossuiAtivo);
+const filtro2 = Array.prototype.filter.apply(li4, [itemPossuiAtivo]);
+
+
+
+
+// APPLY VS CALL
+// A única diferença é a array como segundo argumento.
+
+const li2 = document.querySelectorAll('li');
+
+function itemPossuiAtivo(item) {
+  return item.classList.contains('ativo');
+}
+
+const filtro3 = Array.prototype.filter.call(li2, itemPossuiAtivo);
+const filtro4 = Array.prototype.filter.apply(li2, [itemPossuiAtivo]);
+
+
+
+
+
+// FUNCTION.BIND()
+// Diferente de call e apply, bind(this, arg1, arg2, ...) não irá executar a função mas sim retornar a mesma com o novo contexto de this.
+
+const li3 = document.querySelectorAll('li');
+
+const filtrarLi = Array.prototype.filter.bind(li3, function(item) {
+  return item.classList.contains('ativo');
+});
+
+filtrarLi();
+
+
+
+
+
+
+// ARGUMENTOS E BIND
+// Não precisamos passar todos os argumentos no momento do bind, podemos passar os mesmos na nova função no momento da execução da mesma.
+
+const carro2 = {
+  marca: 'Ford',
+  ano: 2018,
+  acelerar: function(aceleracao, tempo) {
+    return `${this.marca} acelerou ${aceleracao} em ${tempo}`;
+  }
+}
+carro2.acelerar(100, 20);
+// Ford acelerou 100 em 20
+
+const honda = {
+  marca: 'Honda',
+};
+const acelerarHonda = carro2.acelerar.bind(honda);
+acelerarHonda(200, 10);
+// Honda acelerou 200 em 10
+
+
+
+
+
+// ARGUMENTOS COMUNS
+// Podemos passar argumentos padrões para uma função e retornar uma nova função.
+
+function imc(altura, peso) {
+  return peso / (altura * altura);
+}
+
+const imc180 = imc.bind(null, 1.80);
+
+imc(1.80, 70); // 21.6
+imc180(70); // 21.6
+
